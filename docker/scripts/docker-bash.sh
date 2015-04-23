ed3c6fb2ca4d2eb4f1b449dee347503de9e83fe8
Derek Roberts <derek.roberts@gmail.com>
2015-04-22 21:33:38 -0700

    docker-bash.sh now knows when run from VagrantVM

--- docker/scripts/docker-bash.sh
+++ docker/scripts/docker-bash.sh
@@ -5,70 +5,85 @@
 set -e -o nounset
 
 
-# Configure ~/.bashrc
+# If running vagrant use a partiular home directory for .bashrc
 #
-if(! grep --quiet 'function dockin()' ~/.bashrc)
+if [ -d /vagrant/ ] && [ -d /home/vagrant ];
 then
-    echo '' | tee -a ~/.bashrc
-    echo '# Function to make nsenter easier' | tee -a ~/.bashrc
-    echo '#' | tee -a ~/.bashrc
-    echo 'function dockin()' | tee -a ~/.bashrc
-    echo '{' | tee -a ~/.bashrc
-    echo '  if [ $# -eq 0 ]' | tee -a ~/.bashrc
-    echo '  then' | tee -a ~/.bashrc
-    echo '		echo "Please pass a docker container to enter"' | tee -a ~/.bashrc
-    echo '		echo "Usage: dockin [containerToEnter]"' | tee -a ~/.bashrc
-    echo '	else' | tee -a ~/.bashrc
-    echo '		docker exec -it $1 /bin/bash' | tee -a ~/.bashrc
-
-    echo '	fi' | tee -a ~/.bashrc
-    echo '}' | tee -a ~/.bashrc
-fi
-
-if(! grep --quiet 'function reload()' ~/.bashrc)
-then
-    echo '' | tee -a ~/.bashrc
-    echo '# Function to make container rebuilds easier' | tee -a ~/.bashrc
-    echo '#' | tee -a ~/.bashrc
-    echo 'function reload()' | tee -a ~/.bashrc
-    echo '{' | tee -a ~/.bashrc
-    echo '  if [ $# -eq 0 ]' | tee -a ~/.bashrc
-    echo '  then' | tee -a ~/.bashrc
-    echo '    echo "Please pass a docker container to destroy and recreate"' | tee -a ~/.bashrc
-    echo '    echo "Usage: reload [containerToReload]"' | tee -a ~/.bashrc
-    echo '  else' | tee -a ~/.bashrc
-    echo '    CONTAINER=${1%/}' | tee -a ~/.bashrc
-    echo '    docker rm -fv $CONTAINER' | tee -a ~/.bashrc
-    echo '    make build-$CONTAINER' | tee -a ~/.bashrc
-    echo '    make run-$CONTAINER' | tee -a ~/.bashrc
-    echo '  fi' | tee -a ~/.bashrc
-    echo '}' | tee -a ~/.bashrc
-fi
-
-if(! grep --quiet "alias c='dockin'" ~/.bashrc)
-then
-    echo '' | tee -a ~/.bashrc
-    echo '# Aliases to frequently used functions and applications' | tee -a ~/.bashrc
-    echo '#' | tee -a ~/.bashrc
-    echo "alias c='dockin'" | tee -a ~/.bashrc
-    echo "alias d='docker'" | tee -a ~/.bashrc
-    echo "alias r='reload'" | tee -a ~/.bashrc
-    echo "alias l='docker logs'" | tee -a ~/.bashrc
-    echo "alias dc='docker-compose'" | tee -a ~/.bashrc
-fi
-
-# Start in /vagrant/, instead of ~/
-if(! grep --quiet 'cd /vagrant/' ~/.bashrc)
-then
-    echo '' | tee -a ~/.bashrc
-    echo '# Start in /vagrant/, instead of ~/' | tee -a ~/.bashrc
-    echo 'cd /vagrant/docker/' | tee -a ~/.bashrc
+  echo "Vagrant!"
+  HOME=/home/vagrant
+else
+  echo "Not vagrant!"
+  HOME=~
 fi
 
 
-# Set up ~/.vimrc
+# Configure $HOME/.bashrc
 #
-if(! grep --quiet 'function dockin()' /home/vagrant/.vimrc) then
-    echo 'set number' | tee -a /home/vagrant/.vimrc
-    echo 'colorscheme delek' | tee -a /home/vagrant/.vimrc
+if(! grep --quiet 'function dockin()' $HOME/.bashrc)
+then
+    echo '' | tee -a $HOME/.bashrc
+    echo '# Function to make nsenter easier' | tee -a $HOME/.bashrc
+    echo '#' | tee -a $HOME/.bashrc
+    echo 'function dockin()' | tee -a $HOME/.bashrc
+    echo '{' | tee -a $HOME/.bashrc
+    echo '  if [ $# -eq 0 ]' | tee -a $HOME/.bashrc
+    echo '  then' | tee -a $HOME/.bashrc
+    echo '		echo "Please pass a docker container to enter"' | tee -a $HOME/.bashrc
+    echo '		echo "Usage: dockin [containerToEnter]"' | tee -a $HOME/.bashrc
+    echo '	else' | tee -a $HOME/.bashrc
+    echo '		docker exec -it $1 /bin/bash' | tee -a $HOME/.bashrc
+
+    echo '	fi' | tee -a $HOME/.bashrc
+    echo '}' | tee -a $HOME/.bashrc
+fi
+
+if(! grep --quiet 'function reload()' $HOME/.bashrc)
+then
+    echo '' | tee -a $HOME/.bashrc
+    echo '# Function to make container rebuilds easier' | tee -a $HOME/.bashrc
+    echo '#' | tee -a $HOME/.bashrc
+    echo 'function reload()' | tee -a $HOME/.bashrc
+    echo '{' | tee -a $HOME/.bashrc
+    echo '  if [ $# -eq 0 ]' | tee -a $HOME/.bashrc
+    echo '  then' | tee -a $HOME/.bashrc
+    echo '    echo "Please pass a docker container to destroy and recreate"' | tee -a $HOME/.bashrc
+    echo '    echo "Usage: reload [containerToReload]"' | tee -a $HOME/.bashrc
+    echo '  else' | tee -a $HOME/.bashrc
+    echo '    CONTAINER=${1%/}' | tee -a $HOME/.bashrc
+    echo '    docker rm -fv $CONTAINER' | tee -a $HOME/.bashrc
+    echo '    make build-$CONTAINER' | tee -a $HOME/.bashrc
+    echo '    make run-$CONTAINER' | tee -a $HOME/.bashrc
+    echo '  fi' | tee -a $HOME/.bashrc
+    echo '}' | tee -a $HOME/.bashrc
+fi
+
+if(! grep --quiet "alias c='dockin'" $HOME/.bashrc)
+then
+    echo '' | tee -a $HOME/.bashrc
+    echo '# Aliases to frequently used functions and applications' | tee -a $HOME/.bashrc
+    echo '#' | tee -a $HOME/.bashrc
+    echo "alias c='dockin'" | tee -a $HOME/.bashrc
+    echo "alias d='docker'" | tee -a $HOME/.bashrc
+    echo "alias r='reload'" | tee -a $HOME/.bashrc
+    echo "alias l='docker logs'" | tee -a $HOME/.bashrc
+    echo "alias dc='docker-compose'" | tee -a $HOME/.bashrc
+fi
+
+
+# Start in /vagrant/, instead of $HOME/
+#
+if(! grep --quiet 'cd /vagrant/' $HOME/.bashrc)
+then
+    echo '' | tee -a $HOME/.bashrc
+    echo '# Start in /vagrant/, instead of $HOME/' | tee -a $HOME/.bashrc
+    echo 'cd /vagrant/docker/' | tee -a $HOME/.bashrc
+fi
+
+
+# Set up $HOME/.vimrc
+#
+if(! grep --quiet 'function dockin()' $HOME/.vimrc)
+then
+    echo 'set number' | tee -a $HOME/.vimrc
+    echo 'colorscheme delek' | tee -a $HOME/.vimrc
 fi
