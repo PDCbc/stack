f01cd34aa32022de2ad8cd53daeade8a125a1b4f
Derek Roberts <derek.roberts@gmail.com>
2015-04-23 11:17:28 -0700

    Makefile and post-docker config now working

--- docker/scripts/config.sh
+++ docker/scripts/config.sh
@@ -21,18 +21,14 @@ mongoimport --port 27019 --db query_composer_development --collection users     
 #
 mongoimport --port 27020 --db query_gateway_development --collection records $DIR/data/oscar.json
 mongoimport --port 27021 --db query_gateway_development --collection records $DIR/data/osler.json
+#rm $DIR/data/osler.json
 
 
 # Add endpoints to auth
 #
 EP0=`mongo --port 27019 query_composer_development --eval 'printjson( db.endpoints.findOne({base_url:"http://10.0.2.2:40000" }, { "_id":1 }))'  | grep -o "(.*)" | grep -io "\w\+"`
 JS0="{\"clinician\":\"cpsid\",\"clinic\":\""$EP0"\"}"
-docker exec docker_auth_1 /usr/bin/dacspasswd -uj TEST -pds $JS0 oscar
+docker exec app_auth_1 /usr/bin/dacspasswd -uj TEST -pds $JS0 oscar
 EP1=`mongo --port 27019 query_composer_development --eval 'printjson( db.endpoints.findOne({base_url:"http://10.0.2.2:40001" }, { "_id":1 }))'  | grep -o "(.*)" | grep -io "\w\+"`
 JS1="{\"clinician\":\"27542\",\"clinic\":\""$EP1"\"}"
-docker exec docker_auth_1 /usr/bin/dacspasswd -uj TEST -pds $JS1 osler
-
-
-# Restart hub
-#
-#docker restart hub
+docker exec app_auth_1 /usr/bin/dacspasswd -uj TEST -pds $JS1 osler
