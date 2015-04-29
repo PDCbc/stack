39be8162ceafe3c4b49e4d86325171c840f4338c
Derek Roberts <derek.roberts@gmail.com>
2015-04-28 19:35:02 -0700

    Package install and .bashrc config now handled by Makefile, not Vagrant setup

---
+++ docker/scripts/package-config.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+# Exit on errors or unitialized variables
+#
+set -e -o nounset
+
+
+# Vagrant VM only - use apt-get non-interactive
+#
+if [ -d /vagrant/ ]&&[ -d /home/vagrant ];
+then
+  echo "Vagrant!"
+  export DEBIAN_FRONTEND=noninteractive
+else
+  echo "Not vagrant!"
+fi
+
+
+# Update and upgrade, non-interactive
+#
+sudo apt-get update
+sudo apt-get upgrade -y
+
+
+# Create list of packages and then install
+#
+declare -a APPS=( linux-image-extra-`uname -r` \
+                  curl \
+                  mongodb \
+                  nodejs
+                )
+for a in ${APPS[@]}
+do
+  # suppress stdout, show errors
+  (
+#    ( dpkg -l | grep $a )|| sudo apt-get install -y $a
+    ( which $a )|| sudo apt-get install -y $a
+  ) 2>&1 >/dev/null
+
+  if(! dpkg -l | grep $a )
+  then
+    # send output to stderr (red)
+    echo "ERROR:" $a "install failed!" >&2
+  fi
+done
+
+
+# Install the most recent version of docker (requires aufs loaded)
+#
+if( type -p docker )
+then
+  echo "Docker is already installed"
+else
+  # suppress stdout, show errors
+  (
+    modprobe aufs
+    curl https://get.docker.com/ > docker_install.sh
+    sudo sh docker_install.sh
+    rm docker_install.sh
+    gpasswd -a $USER docker
+  ) 2>&1 >/dev/null
+
+  if( type -p docker )
+  then
+    echo "Docker successfully installed"
+  else
+    # send output to stderr (red)
+    echo "ERROR: Docker install failed!" >&2
+  fi
+fi
+
+
+# Install docker-compose
+#
+if( type -p docker-compose )
+then
+  echo "Docker Compose is already installed"
+else
+  # suppress stdout, show errors
+  (
+    sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    #curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > docker-compose.tmp
+    #sudo mv docker-compose.tmp /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  ) 2>&1 >/dev/null
+  if( type -p docker-compose )
+  then
+    echo "Docker Compose successfully installed"
+  else
+    # send output to stderr (red)
+    echo "ERROR: Docker Compose install failed!" >&2
+  fi
+fi
