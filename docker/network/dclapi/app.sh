0ce1399df24c19725d1943cc69b85c0bc16684d2
Derek Roberts <derek.roberts@gmail.com>
2015-05-14 15:32:04 -0700

    Startup scripts able to handle non-empty clone directories

--- docker/network/dclapi/app.sh
+++ docker/network/dclapi/app.sh
@@ -16,6 +16,7 @@ BRANCH=${BRANCH_DLAPI}
 
 # Clone and checkout branch or tag
 #
+( rm -rf /tmp/app/ )|| true
 git clone -b ${BRANCH} --single-branch https://github.com/${REPO} /tmp/app/
 mv /tmp/app/* /app/
 rm -rf /tmp/app/
