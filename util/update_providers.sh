1eb7e3a119c35bc93fbdeb90d5263fe284582f8c
sdiemert <simon.diemert@gmail.com>
2015-10-08 00:08:28 +0000

    added update_providers.sh script and template providers.csv file

--- util/update_providers.sh
+++ util/update_providers.sh
@@ -10,6 +10,7 @@ OLDIFS=$IFS #store old file seperator just in case.
 IFS=","  #set our file seperator to comma
 
 GROUPS_FILE="/pdc/data/config/groups/groups.json"
+JURI="TEST"
 
 count=0
 provider_hash=0
@@ -31,12 +32,27 @@ do
 
 
 	#update the groups.json for providers
-        
 	sudo ./add_user_to_group.py $provider_hash "\""$group"\"" $GROUPS_FILE
 
+	#update the user in dacs
+	sudo docker exec auth /app/manage_users.sh $username $provider_hash $group $JURI
 
+	#update the user to filter providers function in queries via the HAPI container.
+	if [ $attachment != 0 ]; then 
+		sudo docker exec hapi node /app/util/update_filter_providers.js $provider_hash Attachment 
+	fi
+	if [ $pphrr != 0 ]; then 
+		sudo docker exec hapi node /app/util/update_filter_providers.js $provider_hash PPhRR 
+	fi
+	if [ $populationhealth != 0 ]; then 
+		sudo docker exec hapi node /app/util/update_filter_providers.js $provider_hash PopulationHealth 
+	fi
+	if [ $practicereflection != 0 ]; then 
+		sudo docker exec hapi node /app/util/update_filter_providers.js $provider_hash PracticeReflection
+	fi
 
 	count=$((count+1))
+
 done < $1
 
 IFS=$OLDIFS # make sure we set the file seperator back.
