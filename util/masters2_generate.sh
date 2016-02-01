60841cb1258b85d364f2e4138ffd4eedc3ffcdbc
Derek Roberts <derek.roberts@gmail.com>
2016-02-01 13:51:21 -0800

    masters2_generate.sh - updating providers before running reports

--- util/masters2_generate.sh
+++ util/masters2_generate.sh
@@ -5,6 +5,17 @@
 set -e -o nounset
 
 
+# Find and chanbge to script directory
+#
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+
+# Make sure providers are up to date
+#
+./providers.sh update
+
+
 # 5. Run retroImporter.js
 #
 sudo docker exec -it composer /bin/bash -c 'cd /app/util/retroImporter/; QUERY_TITLE=PDC-1738 RETRO_QUERY_TITLE=Retro-PDC-1738 node retroImporter.js'
